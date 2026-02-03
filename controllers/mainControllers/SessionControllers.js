ac3d6afcd05d9a738c887e72da920afc274313b8
eethaldevteam123 <eethalnaditsolutions@gmail.com>
2026-02-03 10:52:36 +0530

    session

--- controllers/mainControllers/SessionControllers.js
+++ controllers/mainControllers/SessionControllers.js
@@ -318,7 +318,7 @@ exports.updateSession = async (req, res) => {
       sessionTime,
       sessionFromTime,
       sessionToTime,
-      sessionCount,
+
       sessionStatusId,
       sessionFeedbackPros,
       sessionFeedbackCons,
@@ -355,7 +355,6 @@ exports.updateSession = async (req, res) => {
           sessionFeedbackCons,
           modeOfExercise,
           redFlags,
-          sessionCount,
           homeExerciseAssigned,
           modalitiesList,
           targetArea,
